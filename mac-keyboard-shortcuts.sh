1c808a80328d8c45ac8c5afe0972624a3b554fda
bbourgoine <brent.bourgoine@gmail.com>
2022-02-03 23:19:50 -0500

    Adding comments

--- mac-keyboard-shortcuts.sh
+++ mac-keyboard-shortcuts.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# mac-keyboard-shortcuts.sh - Setup custom keyboard shortcuts
+
+# Add 'Close Tab' shortcut to Safari on Cmd-w - fixes closing the last tab closing the window
 defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.apple.Safari"
 defaults write com.apple.Safari NSUserKeyEquivalents -dict-add "Close Tab" -string "@w"
 
